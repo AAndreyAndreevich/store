4c58bb38eaa00b7550b45a472039e0ce46eca5e7
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2025-03-01 21:36:38 +0300

    +интеграционные тесты для AccountController

--- src/test/java/app/integration/controller/AccountControllerIntegrationTest.java
+++ src/test/java/app/integration/controller/AccountControllerIntegrationTest.java
@@ -1,14 +1,67 @@
 package app.integration.controller;
 
+import app.entity.Account;
+import app.service.AccountDetailsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class AccountControllerIntegrationTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private AccountDetailsService accountDetailsService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @Test
+    public void testRegistration_Success() throws Exception {
+        mockMvc.perform(get("/registration"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("registration"))
+                .andExpect(model().attributeExists("account"));
+    }
 
+    @Test
+    public void testRegisterUser() throws Exception {
+        Account account = new Account();
+        account.setUsername("testuser");
+        account.setPassword("password");
+
+        mockMvc.perform(post("/registration")
+                .flashAttr("account", account))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+
+        verify(accountDetailsService, times(1)).registerUser(account);
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+        mockMvc.perform(get("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?logout"));
+    }
 }
